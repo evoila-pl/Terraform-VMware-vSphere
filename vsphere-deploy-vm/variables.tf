4eaccf925b4e116bfba5275659c396102b7af18d
Maciej Lelusz <maciej.lelusz@inleo.pl>
2020-10-02 10:33:18 +0200

    Update variables.tf

--- vsphere-deploy-vm/variables.tf
+++ vsphere-deploy-vm/variables.tf
@@ -1,5 +1,3 @@
-# Provider
-
 variable "vsphere_user" {
   type = string
   description = "Username of admin/root account."
@@ -28,18 +26,6 @@ variable "cluster_name" {
 variable "esx_pass" {
   type = string
   description = "Password of root user."
-}
-
-variable "hosts" {
-  default = [
-      "esx",
-  ]
-}
-
-variable "thumbprints" {
-  default = [
-      "4F:6D:22:38:97:CA:25:04:D8:16:D4:87:CF:D8:80:B8:94:53:CC:FF",
-  ]
 }
 
 variable "rp_name" {
