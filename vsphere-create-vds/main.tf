2df9bda12ae5eb74d50c5c585fcc51af35bd3568
Maciej Lelusz <maciej.lelusz@inleo.pl>
2020-10-02 10:29:17 +0200

    Update main.tf

--- vsphere-create-vds/main.tf
+++ vsphere-create-vds/main.tf
@@ -5,57 +5,42 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-resource "vsphere_datacenter" "dc" {
+data "vsphere_datacenter" "dc" {
   name = var.dc_name
 }
 
-resource "vsphere_compute_cluster" "cluster" {
-  name = var.cluster_name
-  datacenter_id = vsphere_datacenter.dc.moid
-
-  drs_enabled = true
-  drs_automation_level = "fullyAutomated"
-
-  ha_enabled = false
-}
-
-resource "vsphere_host" "hosts" {
+data "vsphere_host" "host" {
   count = length(var.hosts)
-
-  hostname = var.hosts[count.index]
-  username = "root"
-  password = var.esx_pass
-  cluster = vsphere_compute_cluster.cluster.id
-  thumbprint = var.thumbprints[count.index]
-}
-
-resource "vsphere_resource_pool" "rp" {
-  name = var.rp_name
-  parent_resource_pool_id = vsphere_compute_cluster.cluster.resource_pool_id
+  name = var.hosts[count.index]
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 resource "vsphere_distributed_virtual_switch" "vds" {
-  count = length(var.hosts)
-
-  name = "vds-test"
-  datacenter_id = vsphere_datacenter.dc.moid
+  name = "vds"
+  datacenter_id = data.vsphere_datacenter.dc.id
   
   uplinks         = ["uplink1", "uplink2", "uplink3", "uplink4"]
   active_uplinks  = ["uplink1", "uplink2"]
   standby_uplinks = ["uplink3", "uplink4"]
 
-  host {    
-    host_system_id = vsphere_host.hosts[count.index].id
+  host {
+    host_system_id = data.vsphere_host.host.0.id
     devices = [
       "vmnic1",
     ]
   }
+
+  host {
+    host_system_id = data.vsphere_host.host.1.id
+    devices = [
+      "vmnic1",
+    ]
+  }
+
 }
 
 resource "vsphere_distributed_port_group" "pg" {
-  count = length(vsphere_distributed_virtual_switch.vds)
-
   name = "frontend"
-  distributed_virtual_switch_uuid = vsphere_distributed_virtual_switch.vds[count.index].id
+  distributed_virtual_switch_uuid = vsphere_distributed_virtual_switch.vds.id
   vlan_id = 0
 }
