4c40df68d12826aad290d7f866675bbbed02708e
Maciej Lelusz <maciej.lelusz@inleo.pl>
2020-10-02 10:22:25 +0200

    Update main.tf

--- vsphere-add-hosts-to-cluster/main.tf
+++ vsphere-add-hosts-to-cluster/main.tf
@@ -19,24 +19,13 @@ resource "vsphere_compute_cluster" "cluster" {
   ha_enabled = false
 }
 
-variable "hosts" {
-  default = [
-      "esx",
-  ]
-}
-
-variable "thumbprints" {
-  default = [
-      "4F:6D:22:38:97:CA:25:04:D8:16:D4:87:CF:D8:80:B8:94:53:CC:FF",
-  ]
-}
-
 resource "vsphere_host" "hosts" {
   count = length(var.hosts)
 
   hostname = var.hosts[count.index]
   username = "root"
   password = var.esx_pass
+  license = "XXXXX-XXXXX-XXXXX-XXXXX-XXXXX"
   cluster = vsphere_compute_cluster.cluster.id
   thumbprint = var.thumbprints[count.index]
 }
